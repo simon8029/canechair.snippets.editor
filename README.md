0733772cabba89076c99477d5cda4a8878c2195f
simon X.L <simon.lau@msn.com>
2018-01-19 14:56:19 -0500

    reset

--- README.md
+++
@@ -1,1 +0,0 @@
-# canechair.snippets.editor
--- src/reducers/rootReducer.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from 'redux';
-import userManagementReducer from './userManagementReducer';
-
-const rootReducer = combineReducers({ userManagementReducer })
-
-export default rootReducer;
